2cc6d0d2a17529fe18e7403efe88a58278601a3b
Raphael Willian <raphael.wcosta@gmail.com>
2015-02-20 21:24:25 -0200

    login working

--- backend/appengine/routes/home.py
+++ backend/appengine/routes/home.py
@@ -2,14 +2,15 @@
 from __future__ import absolute_import, unicode_literals
 from config.template_middleware import TemplateResponse
 from gaecookie.decorator import no_csrf
-from gaepermission.decorator import login_not_required
+from gaepermission.decorator import login_not_required, login_required
 from routes.login.home import prepare_login_services
 from tekton import router
 
 
-@login_not_required
+@login_required
 @no_csrf
-def index(ret_path="/"):
+def index(_logged_user, ret_path="/"):
     dct = prepare_login_services(ret_path)
+    # dct['_logged_user'] = _logged_user
     return TemplateResponse(dct)
 
