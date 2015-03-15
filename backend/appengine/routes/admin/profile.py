ab18a9b69bf2a99df7a2005770ea10b14f5d3257
Raphael Willian <raphael.wcosta@gmail.com>
2015-03-15 12:03:10 -0300

    building a admin profile page

---
+++ backend/appengine/routes/admin/profile.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from gaecookie.decorator import no_csrf
+from config.template_middleware import TemplateResponse
+
+
+@no_csrf
+def index():
+    return TemplateResponse()
