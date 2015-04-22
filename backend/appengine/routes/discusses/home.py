7d0f9b45e8f9b0f5d058156d98b1bf5ce6c592fd
Raphael Willian <raphael.wcosta@gmail.com>
2015-04-21 22:42:25 -0300

    get discuss by id

--- backend/appengine/routes/discusses/home.py
+++ backend/appengine/routes/discusses/home.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from config.template_middleware import TemplateResponse
+from gaepermission.model import MainUser
+from permission_app.permission_facade import main_user_form
 from tekton import router
 from gaecookie.decorator import no_csrf
 from discuss_app import discuss_facade
 from tekton.gae.middleware.redirect import RedirectResponse
-from datetime import datetime
 
 
 @no_csrf
@@ -13,47 +14,18 @@ def index(discuss=""):
     context = {"discusses_page": router.to_path(index)}
     if discuss:
         cmd = discuss_facade.get_discuss_cmd(discuss)
+        form = discuss_facade.discuss_form()
+
         query_discuss = cmd()
-        context['discuss'] = query_discuss
+        discuss = form.fill_with_model(query_discuss)
+        discuss['publisher'] = main_user_form().fill_with_model(MainUser.get_by_id(int(discuss['user'])))
+
+        context['discuss'] = discuss
         return TemplateResponse(template_path="discusses/discuss.html", context=context)
-    # query = Discuss.query().order(Discuss.creation).fetch()[:10][::-1]
     cmd = discuss_facade.ListDiscussCommand()
     discusses = cmd()
-    query = [
-        {
-            "key": 54323122,
-            "title": "Lorem Ipsum San Shi",
-            "content": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam",
-            "creation": datetime.now() - datetime.now(),
-            "image": "/static/img/python.jpg"
-        },
-        {
-            "key": 54323154,
-            "title": "Lorem Ipsum San Shi",
-            "content": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam",
-            "creation": datetime.now() - datetime.now(),
-            "image": "/static/img/python.jpg"
-        }
-    ]
     context['discusses'] = discusses
     return TemplateResponse(context, template_path="discusses/home.html")
-
-    # cmd = discuss_facade.list_discusss_cmd()
-    # discusss = cmd()
-    # edit_path = router.to_path(edit)
-    # delete_path = router.to_path(delete)
-    # discuss_form = discuss_facade.discuss_form()
-    #
-    # def localize_discuss(discuss):
-    #     discuss_dct = discuss_form.fill_with_model(discuss)
-    #     discuss_dct['edit_path'] = router.to_path(edit_path, discuss_dct['id'])
-    #     discuss_dct['delete_path'] = router.to_path(delete_path, discuss_dct['id'])
-    #     return discuss_dct
-    #
-    # localized_discusss = [localize_discuss(discuss) for discuss in discusss]
-    # context = {'discusss': localized_discusss,
-    #            'new_path': router.to_path(new)}
-    # return TemplateResponse(context, 'discusss/discuss_home.html')
 
 
 def delete(discuss_id):
